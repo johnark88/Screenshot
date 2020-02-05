a00491e30bf8676ed669521e0efc78bf51a4517f
John Arkema <john.arkema@gmail.com>
2020-02-04 19:04:23 -0800

    add some eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,6 +10,11 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'max-len': ['warn', { code: 100 }],
+    'eqeqeq': ["warn", "smart"],
+    'no-empty-function': "error",
+    'no-multi-spaces': "error",
+    'no-param-reassign': "error"
   },
   parserOptions: {
     parser: 'babel-eslint',
