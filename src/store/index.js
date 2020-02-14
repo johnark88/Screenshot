472ad2df31d00fbbec6b2321f6bed488c15f7602
johnark88 <john.arkema@gmail.com>
2020-02-14 07:04:52 -0800

    Getters / store object

--- src/store/index.js
+++ src/store/index.js
@@ -5,15 +5,28 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-    imgData: {
-      name: '',
-      src: '',
+    screenshots: {
+      screenshotData: [{
+        name: '',
+        src: '',
+        status: '',
+      }],
     },
   },
+  /* eslint-disable */
+  getters: {
+    successfulShot: (state) => {
+      console.log(state, 'state');
+      console.log(state.screenshots.screenshotData.filter(shot => shot.status === true));
+    },
+  },
+  /* eslint-enable */
   mutations: {
     addImageData(state, payload) {
-      state.imgData.src = payload.src;
-      state.imgData.name = payload.name;
+      console.log(payload, 'payload');
+      state.screenshots.status = payload.status;
+      state.screenshots.src = payload.file;
+      state.screenshots.name = payload.name;
     },
   },
   actions: {
