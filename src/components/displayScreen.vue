0fea3a1bee9728c636bdb38a4669df094ace1dac
johnark88 <john.arkema@gmail.com>
2020-02-19 21:45:30 -0800

    Display Image - :src not working

--- src/components/displayScreen.vue
+++ src/components/displayScreen.vue
@@ -1,33 +1,48 @@
 <template>
   <div class="display-screen">
-    <!-- <img src="{{imageURL}}" alt=""> -->
-    <button> here</button>
-    {{screenShots}}
+    <div class="image-display p-10">
+      <div class="image-display__image bg-blue text-cener">
+        <div class="p-4 m-2 text-white" v-for="(shot, index) in screenShots" v-bind:key="index">
+          <h3>Site Name: {{shot.screenshot.siteName}}</h3>
+          <h4>File Name: {{shot.screenshot.fileName}}</h4>
+          <img v-bind:src="'/Users/johnA/Documents/Building/screenshot' + shot.screenshot.fileURL" alt="">
+          <img src="https://i.stack.imgur.com/SRH0B.png" alt="">
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'display-screen',
+  props: {
+    screenshot: {
+      success: Boolean,
+      fileURL: String,
+      siteName: String,
+      fileName: String,
+    },
+  },
   data() {
     return {
       thing: null,
-      // imageURL: '',
-      successfulShots: [],
+      screenShots: [],
     };
   },
   watch: {
-    screenShots(newVal, oldVal) {
-      if (newVal === null || newVal === undefined) {
-        console.log('no', oldVal);
-      }
-      console.log('yes', newVal);
+    /* eslint-disable */
+    screenshot: {
+      handler: function(oldVal, newVal) {
+        if (newVal) {
+          const screenshot = newVal;
+          console.log(newVal, 'newVal');
+          this.screenShots.push({screenshot});
+        }
+      },
+      deep: true,
     },
-  },
-  computed: {
-    screenShots() {
-      return this.$store.getters.successfulShot;
-    },
+    /* eslint-enable */
   },
 };
 </script>
